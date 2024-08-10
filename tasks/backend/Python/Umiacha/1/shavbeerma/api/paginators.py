b6c15ce862884202aba1c9d01e382dd7db96b116
Sulaev Vladimir <umiacha@yandex.ru>
2024-08-10 20:24:05 +0300

    Replace pagination settings

--- tasks/backend/Python/Umiacha/1/shavbeerma/api/paginators.py
+++ tasks/backend/Python/Umiacha/1/shavbeerma/api/paginators.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class StuffPagination(PageNumberPagination):
     """Пагинатор для товаров."""
-    page_size = 10  # TODO: перенести эту константу в настройки (есчо, можно будет наследовать другой пагинатор от этого и переписать).
     page_size_query_param = 'limit'
--- tasks/backend/Python/Umiacha/1/shavbeerma/api/views.py
+++ tasks/backend/Python/Umiacha/1/shavbeerma/api/views.py
@@ -9,16 +9,15 @@ from .serializers import BeerSerializer, ShaurmaSerializer
 
 class StuffViewSet(ModelViewSet):
     """Базовый класс для товаров."""
+    http_method_names = ['get', ]
     pagination_class = StuffPagination
 
 
 class BeerViewSet(StuffViewSet):
     queryset = Beer.objects.all()
-    http_method_names = ['get', ]
     serializer_class = BeerSerializer
 
 
 class ShaurmaViewSet(StuffViewSet):
     queryset = Shaurma.objects.all()
-    http_method_names = ['get', ]
     serializer_class = ShaurmaSerializer
