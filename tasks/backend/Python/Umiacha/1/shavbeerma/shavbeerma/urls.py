0777803f6d1102796243f3f0415df71ae228fd4e
Sulaev Vladimir <umiacha@yandex.ru>
2024-08-11 14:27:55 +0300

    Add redoc and swagger

--- tasks/backend/Python/Umiacha/1/shavbeerma/shavbeerma/urls.py
+++ tasks/backend/Python/Umiacha/1/shavbeerma/shavbeerma/urls.py
@@ -1,16 +1,22 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-# TODO: сгенерировать документацию и прикрутить её в режиме дебага.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
-# if settings.DEBUG:  # Вариант раздачи статики для дебага.
-#     urlpatterns += static(
-#         settings.MEDIA_URL,
-#         document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(
-#         settings.STATIC_URL,
-#         document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += [path('', include('docs.urls')),]
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT)
